69d671e35a39ff89ecbe73b0c1f4fed0f09f7bdc
yolisett <lizbeth9589@gmail.com>
2017-02-04 21:09:58 -0500

    Terminado y diferente codigo Js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
-function mostrarMensaje(_idInput, _mensaje){
-    var elemento = document.getElementById(_idInput);
-    var span = elemento.parentNode.getElementsByTagName("span");
-    if (span.length === 0) {
-        console.log("se creo")
-        span = document.createElement("span");
-        elemento.parentNode.appendChild(span);
+//Creo un Span.y verifica si esta creado mi span 
+function mostrarMensaje(input, _mensaje, mode=false){
+    var span = input.parentNode.getElementsByTagName("span");
+    if (mode) {
+        if (span.length === 0) {
+            span = document.createElement("span");
+            input.parentNode.appendChild(span);
+        }
+        span.innerHTML = _mensaje;
+        input.parentNode.getElementsByTagName("span")[0].style.display="block";
     }
-    span.innerHTML = _mensaje;
+    else if (span.length !== 0) {
+        input.parentNode.getElementsByTagName("span")[0].style.display="none";
+    }
 }
-
 function validateForm(){
     nombres();
     apellidos();
@@ -17,75 +21,111 @@ function validateForm(){
     seleccionar();
     mensaje();
 }
-function nombres(){
-   console.log("hola llegue");
+//Codigo para name lastname , email password
+function validate_input(input) {
+    console.log(input);
+    var validations = {
+        "name": /^[A-Z][a-z]+$/,
+        "lastname": /^[A-Z][a-z]+$/,
+        "email": /^\S+@\S+\.\S+$/,
+    };
+    if (validations[input.id].test(input.value) == false) {
+        return false;
+    }
+    else
+        return true;
+}
+function nombres() {
     var name = document.getElementById("name");
-    if (/^[A-Z][a-z]+$/.test(name.value) == false){
-       mostrarMensaje("name", "Incorrecto, La primera letra debe ser Mayúscula");
-        name.parentNode.getElementsByTagName("span")[0].style.display="block"; 
-   } else{
-       console.log(name.nextSibling);
-       name.parentNode.getElementsByTagName("span")[0].style.display="none";
-   }
+    if (validate_input(name) == false){
+        mostrarMensaje(
+            name, 
+            "Incorrecto, La primera letra debe ser Mayúscula", 
+            true
+        );
+    } else {
+        mostrarMensaje(name);
+    }
 }
 function apellidos(){
-    console.log("hola llegue");
     var lastname = document.getElementById("lastname");
-   if (/^[A-Z][a-z]+$/.test(lastname.value) == false){
-       mostrarMensaje("lastname", "Incorrecto, La primera letra debe ser Mayúscula y sin números");
-       lastname.parentNode.getElementsByTagName("span")[0].style.display="block";
-   } else{
-       console.log(name.nextSibling);
-        lastname.parentNode.getElementsByTagName("span")[0].style.display="none";
-   }
+    if (validate_input(lastname) == false){
+        mostrarMensaje(
+            lastname, 
+            "Incorrecto, La primera letra debe ser Mayúscula y sin números",
+            true
+        );
+    } else{
+        mostrarMensaje(lastname);
+    }
 }
 function correo(){
-    console.log("hola llegue");
     var email = document.getElementById("email");
-   if (/^\S+@\S+\.\S+$/.test(email.value) == false){
-       mostrarMensaje("email", 'email incorrecto');
-       email.parentNode.getElementsByTagName("span")[0].style.display="block";
+   if (validate_input(email) == false){
+        mostrarMensaje(
+            email, 
+            'email incorrecto',
+            true
+        );
    } else{
-       console.log(name.nextSibling);
-        email.parentNode.getElementsByTagName("span")[0].style.display="none";
+        mostrarMensaje(email);
    }
 } 
 function validatePassword(){
-    console.log("hola llegue");
     var password = document.getElementById("password");
    if ((password.value).length>=6 === false || (/^[0-9]+$/.test(password.value) !== false)|| (/^[a-zA-Z]+$/.test(password.value) !== false) ){
-       mostrarMensaje("password", 'El password debe tener 6 carácteres con letras y números');
-       password.parentNode.getElementsByTagName("span")[0].style.display="block";
+       mostrarMensaje(
+           password, 
+           'El password debe tener 6 carácteres con letras y números', 
+           true
+       );
 	} else{
-        password.parentNode.getElementsByTagName("span")[0].style.display="none";
+        mostrarMensaje(password);
     }
 }
 function seleccionar(){
     var control = document.getElementById("control");
     if((control.value)==0 !==false ){
-        mostrarMensaje("control", 'Selecciona una opción');
-        control.parentNode.getElementsByTagName("span")[0].style.display="block";
+        mostrarMensaje(
+            control, 
+            'Selecciona una opción', 
+            true
+        );
     } else{
-        control.parentNode.getElementsByTagName("span")[0].style.display="none";
+        mostrarMensaje(control);
     }
 }
- function mensaje(){
-    var lastname = document.getElementById("name");
+//Creo un div , el cual avisara que se tiene que llenar todo 
+function manageMessage(_idInput, _mensaje, mode=false){
+    var elemento = document.getElementById(_idInput);
+    //verifica si esta creado mi div 
+    if (mode) {
+        if ( ! elemento) {
+            elemento = document.createElement("div");
+            elemento.id = _idInput;
+            elemento.classList.add("caja1");
+        }
+        elemento.innerHTML = _mensaje;        
+        elemento.style.display="block";
+        
+        var myForm = document.getElementById("myForm");
+        myForm.appendChild(elemento);
+    }
+    else {
+        var message = document.getElementById("mia");
+        message.style.display="none";
+    }
+}
+ function mensaje() {
+    var name = document.getElementById("name");
     var lastname = document.getElementById("lastname");
     var email = document.getElementById("email");
     var password = document.getElementById("password");
     var control = document.getElementById("control");
-    var mensajes= document.getElementById("mensajes");
     if((name.value)=="" !==false || (lastname.value)=="" !==false || (email.value)=="" !==false || (password.value)=="" !==false || (control.value)==0 !==false){
-    mostrarMensaje("mensajes", 'Es obligatorio, llenar los datos porfavor!!');
-        mensajes.parentNode.getElementsByTagName("span")[0].style.display="block";
+        manageMessage("mia", 'Es obligatorio, llenar los datos porfavor!!', true);
         
-    } else{
-        mensajes.parentNode.getElementsByTagName("span")[0].style.display="none";
-        
+    } else {
+        manageMessage();
     }
 }
-
-
-
-
